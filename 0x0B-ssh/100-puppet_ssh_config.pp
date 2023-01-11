5e5626d60ac835e0a913c3a2fe2ee30b1e87bb12
Ousman <usman.traid@gmail.com>
2023-01-11 11:59:18 -0500

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@
 include stdlib
 file_line { 'Turn off passwd auth':
   ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
+  path   => '~/.ssh/config',
   line   => '    PasswordAuthentication no',
   match  => '#   PasswordAuthentication yes',
   }
 
 file_line { 'Declare identity file':
   ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
+  path   => '~/.ssh/config',
   line   => '    IdentityFile ~/.ssh/schhol',
   match  => '#   IdentityFile ~/.ssh/id_ed25519',
   }
