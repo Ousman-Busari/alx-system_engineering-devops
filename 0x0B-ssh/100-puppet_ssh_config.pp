20fcde7a8090d32071150a829a6ccc9605d605d1
Ousman <usman.traid@gmail.com>
2023-01-11 11:56:35 -0500

    change resource identifier

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,11 +5,12 @@ file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-  match  => '#   PasswordAuthentication yes'
+  match  => '#   PasswordAuthentication yes',
   }
 
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/schhol',
+  match  => '#   IdentityFile ~/.ssh/id_ed25519',
   }
