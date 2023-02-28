7898e5fadbfa82adc0b6c7d5706260f3999ef1aa
Ousman <usman.traid@gmail.com>
2023-02-28 06:17:55 -0500

    requirement fixed

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-100-count_words
+100-count
 """
 import requests
 
@@ -50,3 +50,4 @@ def count_words(subreddit, word_list, count=0, after="", hot_list=[]):
         search_count = sorted(search_count.items(),
                               key=lambda x: (-x[1], x[0]))
         [print("{}: {}".format(s[0], s[1])) for s in search_count if s[1] != 0]
+        return
